282e6bffeef7780295233708789af5d9987cd400
Malcolm Lima <malcolm.lima@sesisenaisp.org.br>
2025-05-08 16:37:21 -0300

    feat: application with cqrs, mediatr and event sourcing

---
+++ src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrdersByCustomerQuery/GetOrderByCustomerQuery.cs
@@ -0,0 +1,4 @@
+﻿namespace Ordering.Application.Orders.Queries.GetOrdersByCustomerQuery;
+public record GetOrdersByCustomerQuery(Guid CustomerId)
+    : IQuery<GetOrdersByCustomerResult>;
+public record GetOrdersByCustomerResult(IEnumerable<OrderDto> Orders);
