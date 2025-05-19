4577e5814cd5bb59b57a9852032529949811555c
Malcolm Lima <malcolm.lima@sesisenaisp.org.br>
2025-05-19 08:08:56 -0300

    feat: web client app with razor, models and services

---
+++ src/WebApps/Shopping.Web/GlobalUsing.cs
@@ -0,0 +1,5 @@
+﻿global using Shopping.Web.Models.Catalog;
+global using Shopping.Web.Models.Basket;
+global using Refit;
+global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.AspNetCore.Mvc.RazorPages;
